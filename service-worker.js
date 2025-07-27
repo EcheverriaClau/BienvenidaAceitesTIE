7df92605825f49147dfc4f8702cf46776f0edcc4
EcheverriaClau <echeverria.cla@gmail.com>
2025-07-27 11:41:21 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,28 @@
 const CACHE_NAME = 'aceites-cache-v1';
-const URLS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/icon-192.png',
-  '/icon-512.png'
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon-192.png',
+  './icon-512.png',
+  './manifest.json'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
+// Instala y guarda en caché
+self.addEventListener('install', function(event) {
+  event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(URLS_TO_CACHE))
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(response => response || fetch(e.request))
+// Carga desde la caché o desde la red
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
   );
 });
